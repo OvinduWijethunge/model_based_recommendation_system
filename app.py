66a2ade995dc5cf22bbc44f2f4eebdd3a6895c96
Ovindu Wijethunge <oumw.udesh@gmail.com>
2021-06-20 07:42:59 +0530

    built model based rcommendation phase

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ import pickle
 import numpy as np
 
 # Load the model
-filename = 'SVD_model.pkl'
+filename = 'SVD_modelv2.pkl'
 algo = pickle.load(open(filename, 'rb'))
+
+df_rating = pd.read_csv('modified_rating_dataSet.csv')
+df_movies = pd.read_csv('modified_movies_dataSet.csv')
 
 app = Flask(__name__)
 
@@ -19,31 +22,39 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+def get_recommendations(uid):
+    prediction_list = []
+    
+    for mid in df_rating['movieID'].unique():
+        predic = algo.predict(uid,mid)
+        prediction_list.append(predic)
+    return prediction_list  
+
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
     
-    df_new = pd.read_csv('colab_dataSet.csv')
-    df_movies = pd.read_table("dataSet/movies.dat",sep="\t")
+    
     if request.method == 'POST':
     
-        #uid = int(request.form['user_id'])
-        uid = 1123
-        prediction_list = []
-        for mid in df_new['movieID'].unique():
-            predic = algo.predict(uid,mid) 
-            prediction_list.append(predic)
-        dfs_pred=pd.DataFrame(prediction_list)
-        dfs_pred.drop(columns=['details'],inplace=True)
-        dfs_pred=dfs_pred.rename(columns={'uid':'userID', 'iid':'id','est':'rating'})
-        dfs_pred = dfs_pred.sort_values('rating',ascending=False)
-        df_1 = dfs_pred.head(10)
-        df2 = df_1.merge(df_movies ,on='id',how='left')
-        recom_movies_list = df2[['title']].values
-        print(recom_movies_list)
-        #return recom_movies_list
-        #recom_movies_list = df_1[['id']].values
-        return render_template('result.html', prediction=recom_movies_list)
+        uid = int(request.form['user_id'])
+        #uid = 75
+        is_user_availble = df_rating[df_rating['userID'] == uid].shape 
+        if is_user_availble[0] == 0:
+            text = ['This is a new user']
+            return render_template('result.html', prediction=text)
+        else:
+            
+            prediction = get_recommendations(uid)
+            df_pred=pd.DataFrame(prediction)
+            # Renaming our predictions to original names
+            df_pred=df_pred.rename(columns={'uid':'userID', 'iid':'movieID','est':'rating'}).sort_values('rating',ascending=False).head(10)
+            #print(df_pred['movieID'])
+            df_final = df_pred.merge(df_movies ,on='movieID',how='left')
+            movies_list = df_final['title'].values
+            
+            print(movies_list)
+            return render_template('result.html', prediction=movies_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
